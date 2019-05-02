aef239b7028ea012c3c609f12a6b5f317aeab8be
vijayakowsi <43548095+vijayakowsi@users.noreply.github.com>
2019-05-02 17:07:06 +0530

    Create hexal to decimal.java

---
+++ hexal to decimal.java
@@ -0,0 +1,11 @@
+program to convert hexadecimal to decimal
+
+public class HexToDecimalExample1{  
+public static void main(String args[]){  
+String hex="a";  
+int decimal=Integer.parseInt(hex,16);  
+System.out.println(decimal);  
+}}  
+
+Another method:
+
